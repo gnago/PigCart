ffa6021c7a6db4fc4b9a0688752c65ffe61342ea
Gnago <gnagogreen@gmail.com>
2024-12-02 15:40:45 -0500

    Added Acceleration system. This fixes not being able to turn corners nicely and generally feels better and more natural

--- src/me/gnago/pigcart/Main.java
+++ src/me/gnago/pigcart/Main.java
@@ -1,6 +1,7 @@
 package me.gnago.pigcart;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Input;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
@@ -14,7 +15,10 @@ import java.util.ArrayList;
 
 public class Main extends JavaPlugin {
 
-    public ArrayList<Player> PigCartPlayers = new ArrayList<Player>();
+    public ArrayList<Player> PigCartPlayers = new ArrayList<>();
+
+    private final static double ACCELERATION_RATE = 0.025;
+    private final static double MAX_SPEED = 1.2; //powered carts are 8m/s, which is 0.4m/t
 
     @Override
     public void onEnable() {
@@ -27,16 +31,26 @@ public class Main extends JavaPlugin {
                         Entity vehicleCart = vehiclePig.getVehicle();
                         if (vehicleCart != null && vehicleCart.getType().equals((EntityType.MINECART)))
                         {
-                            if (plr.getCurrentInput().isBackward()) {
-                                Vector direction = plr.getFacing().getDirection();
-                                vehicleCart.setVelocity(direction);
-                                plr.sendMessage(ChatColor.GREEN + "Current direction: " + direction.getX() + ", " + direction.getY() + ", " + direction.getZ());
+                            Input currInput = plr.getCurrentInput();
+                            if (currInput.isBackward() || currInput.isForward()) {
+                                Vector direction;
+                                if (currInput.isBackward())
+                                    direction = plr.getFacing().getDirection();
+                                else if (currInput.isForward())
+                                    direction = plr.getFacing().getOppositeFace().getDirection();
+                                else
+                                    return;
+
+                                //Accelerate in the direction
+                                Vector currentVelocity = vehicleCart.getVelocity();
+                                if (currentVelocity.getX() < MAX_SPEED)
+                                    currentVelocity.setX(ACCELERATION_RATE * direction.getX() + currentVelocity.getX());
+                                if (currentVelocity.getZ() < MAX_SPEED)
+                                    currentVelocity.setZ(ACCELERATION_RATE * direction.getZ() + currentVelocity.getZ());
+                                vehicleCart.setVelocity(currentVelocity);
+                                plr.sendMessage(ChatColor.GREEN + "Current velocity: " + currentVelocity.getX() + ", " + currentVelocity.getY() + ", " + currentVelocity.getZ());
                             }
-                            if (plr.getCurrentInput().isForward()) {
-                                Vector direction = plr.getFacing().getOppositeFace().getDirection();
-                                vehicleCart.setVelocity(direction);
-                                plr.sendMessage(ChatColor.RED + "Current direction: " + direction.getX() + ", " + direction.getY() + ", " + direction.getZ());
-                            }
+
                         }
                     }
                 }
@@ -52,7 +66,7 @@ public class Main extends JavaPlugin {
         Bukkit.getLogger().info(ChatColor.RED + "Disabled " + this.getName());
     }
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]) {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player plr = (Player) sender;
 
         if (sender instanceof Player) {
--- src/me/gnago/pigcart/PigCartListener.java
+++ src/me/gnago/pigcart/PigCartListener.java
@@ -1,22 +1,19 @@
 package me.gnago.pigcart;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInputEvent;
 import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
-import org.bukkit.util.Vector;
 
 public class PigCartListener implements Listener {
     public PigCartListener(Main plugin) {
         this.plugin = plugin;
     }
 
-    Main plugin;
+    private final Main plugin;
 
     @EventHandler
     public void OnVehicleEnter(VehicleEnterEvent event) {
@@ -52,21 +49,4 @@ public class PigCartListener implements Listener {
             }
         }
     }
-
-    /*
-    @EventHandler
-    public void OnPlayerInput(PlayerInputEvent event) {
-        Entity vehiclePig = event.getPlayer().getVehicle();
-        if (vehiclePig != null && vehiclePig.getType().equals(EntityType.PIG)) {
-            Entity vehicleCart = vehiclePig.getVehicle();
-            if (vehicleCart != null && vehicleCart.getType().equals((EntityType.MINECART)))
-            {
-                if (event.getInput().isBackward()) {
-                    Vector direction = event.getPlayer().getFacing().getOppositeFace().getDirection();
-                    vehicleCart.setVelocity(direction);
-                    event.getPlayer().sendMessage(ChatColor.GREEN + "Current direction: " + direction.getX() + ", " + direction.getY() + ", " + direction.getZ());
-                }
-            }
-        }
-    }*/
 }
