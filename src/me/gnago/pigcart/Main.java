bd1993b9a23563695921c256cbc6c1272a545c11
Gnago <gnagogreen@gmail.com>
2024-12-02 14:56:54 -0500

    Switched to scheduled repeating task from PlayerInput event listener.
    Added command to see players currently riding carts for debugging

--- src/me/gnago/pigcart/Main.java
+++ src/me/gnago/pigcart/Main.java
@@ -1,13 +1,47 @@
 package me.gnago.pigcart;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
 
 public class Main extends JavaPlugin {
+
+    public ArrayList<Player> PigCartPlayers = new ArrayList<Player>();
+
     @Override
     public void onEnable() {
         Bukkit.getLogger().info(ChatColor.GREEN + "Enabled " + this.getName());
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            public void run() {
+                for (Player plr : PigCartPlayers) {
+                    Entity vehiclePig = plr.getVehicle();
+                    if (vehiclePig != null && vehiclePig.getType().equals(EntityType.PIG)) {
+                        Entity vehicleCart = vehiclePig.getVehicle();
+                        if (vehicleCart != null && vehicleCart.getType().equals((EntityType.MINECART)))
+                        {
+                            if (plr.getCurrentInput().isBackward()) {
+                                Vector direction = plr.getFacing().getDirection();
+                                vehicleCart.setVelocity(direction);
+                                plr.sendMessage(ChatColor.GREEN + "Current direction: " + direction.getX() + ", " + direction.getY() + ", " + direction.getZ());
+                            }
+                            if (plr.getCurrentInput().isForward()) {
+                                Vector direction = plr.getFacing().getOppositeFace().getDirection();
+                                vehicleCart.setVelocity(direction);
+                                plr.sendMessage(ChatColor.RED + "Current direction: " + direction.getX() + ", " + direction.getY() + ", " + direction.getZ());
+                            }
+                        }
+                    }
+                }
+            }
+        }, 0, 1);
         PluginManager pm = getServer().getPluginManager();
         PigCartListener listener = new PigCartListener(this);
         pm.registerEvents(listener, this);
@@ -16,5 +50,27 @@ public class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         Bukkit.getLogger().info(ChatColor.RED + "Disabled " + this.getName());
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]) {
+        Player plr = (Player) sender;
+
+        if (sender instanceof Player) {
+            String lowerCmd = cmd.getName().toLowerCase();
+
+            switch (lowerCmd) {
+                case "pigcarters":
+                    plr.sendMessage(ChatColor.LIGHT_PURPLE + "Current PigCarters");
+                    for (Player p : PigCartPlayers) {
+                        plr.sendMessage(p.getName());
+                    }
+                    plr.sendMessage();
+                    return true;
+                default:
+                    return true;
+            }
+        }
+
+        return true;
     }
 }
--- src/me/gnago/pigcart/PigCartListener.java
+++ src/me/gnago/pigcart/PigCartListener.java
@@ -3,16 +3,57 @@ package me.gnago.pigcart;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInputEvent;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.event.vehicle.VehicleExitEvent;
 import org.bukkit.util.Vector;
 
 public class PigCartListener implements Listener {
     public PigCartListener(Main plugin) {
-
+        this.plugin = plugin;
     }
 
+    Main plugin;
+
+    @EventHandler
+    public void OnVehicleEnter(VehicleEnterEvent event) {
+        Entity plr = event.getEntered();
+        if (plr.getType().equals(EntityType.PLAYER))
+        {
+            Entity pig = event.getVehicle();
+            if (pig.getType().equals(EntityType.PIG)) {
+                Entity cart = pig.getVehicle();
+                if (cart != null && cart.getType().equals(EntityType.MINECART)) {
+                    plr.sendMessage("Entered PigCart");
+                    plugin.PigCartPlayers.add((Player)plr);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void OnVehicleExit(VehicleExitEvent event) {
+        Entity exited = event.getExited();
+        if (exited.getType().equals(EntityType.PLAYER))
+        {
+            Player plr = (Player)exited;
+            Entity pig = event.getVehicle();
+            if (pig.getType().equals(EntityType.PIG)) {
+                Entity cart = pig.getVehicle();
+                if (cart != null && cart.getType().equals(EntityType.MINECART)) {
+                    plr.sendMessage("Exited PigCart");
+                    if (plugin.PigCartPlayers.removeIf(p -> p.getUniqueId().equals(plr.getUniqueId()))) {
+                        plugin.getServer().broadcastMessage("Removed from list");
+                    }
+                }
+            }
+        }
+    }
+
+    /*
     @EventHandler
     public void OnPlayerInput(PlayerInputEvent event) {
         Entity vehiclePig = event.getPlayer().getVehicle();
@@ -27,5 +68,5 @@ public class PigCartListener implements Listener {
                 }
             }
         }
-    }
+    }*/
 }
